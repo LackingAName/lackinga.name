8660f74134620b77f8ca72e464ed65dd1b05005b
LackingAName <catstovetop@gmail.com>
2025-03-14 20:20:35 -0400

    13.4

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ function LevelDown() {
 
 function LoadUser() {
     var UL = document.getElementById("UserLabel")
-    var Random = (Math.floor(Math.random() * Math.floor(23)) + 1)
+    var Random = (Math.floor(Math.random() * Math.floor(25)) + 1)
     if (Random == UL.innerHTML) {Random += 1}
     document.getElementById("User").src = "images/User/" + Random + ".png"
     UL.innerHTML = Random
