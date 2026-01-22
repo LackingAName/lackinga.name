796db5dd88f2220eeeca5f165c6702c3abc5345c
LackingAName <catstovetop@gmail.com>
2026-01-22 04:36:02 -0500

    21.3

--- js/non.js
+++ js/non.js
@@ -9,65 +9,75 @@ document.addEventListener("DOMContentLoaded", function () {
 
         $.each(JSON, function (_, Data) {
             if (Data.Separator) {
-                // create grid label
-                const Label = document.createElement("div");
-                {
-                    Label.style.color = "#fff";
-                    Label.style.textAlign = "center";
-                    Label.style.fontSize = "64px";
-                    Label.style.marginTop = "80px";
-                    Label.innerHTML = Data.Separator;
-                    Container.appendChild(Label);
-                }
+				// create grid label
+				const Label = document.createElement("div");
+				{
+					Label.style.color = "#fff";
+					Label.style.textAlign = "center";
+					Label.style.fontSize = "64px";
+					Label.style.marginTop = "80px";
+					Label.innerHTML = Data.Separator;
+					Container.appendChild(Label);
+				}
 
-                // set to new grid
-                Grid = document.createElement("div");
-                Grid.className = "nongrid";
-                Container.appendChild(Grid);
+				// set to new grid
+				Grid = document.createElement("div");
+				Grid.className = "nongrid";
+				Container.appendChild(Grid);
 
-                return;
-            }
+				return;
+			}
 
-            const Fav = Data.Favorite ? "fav" : "";
+			const Fav = Data.Favorite ? "fav" : "";
 
-            const nonContainer = document.createElement("div");
-            {
-                const nonImage = document.createElement("img");
-                nonImage.className = Fav;
-                nonImage.setAttribute("onclick", "window.open(src,'_blank');");
-                nonImage.src = Data.Image;
-                nonContainer.appendChild(nonImage);
+			const nonContainer = document.createElement("div");
+			{
+				const nonImage = document.createElement("img");
+				nonImage.className = Fav;
+				nonImage.setAttribute("onclick", "window.open(src,'_blank');");
+				nonImage.src = Data.Image;
+				nonContainer.appendChild(nonImage);
 
-                nonContainer.innerHTML += Data.Name + "<br>" + Data.Date + "<br>";
+				nonContainer.innerHTML += Data.Name + "<br>" + Data.Date + "<br>";
 
-                const Credits = document.createElement("span")
-                Credits.style.cursor = "pointer";
-                Credits.addEventListener("click", function () { CreditsPopup(Data.Credits); });
-                {
-                    const CreditsLabel = document.createElement("b");
-                    CreditsLabel.innerHTML = "view credits";
-                    Credits.appendChild(CreditsLabel);
-                }
-                nonContainer.appendChild(Credits);
+				const Credits = document.createElement("span")
+				Credits.style.cursor = "pointer";
+				Credits.addEventListener("click", function () { CreditsPopup(Data.Credits); });
+				{
+					const CreditsLabel = document.createElement("b");
+					CreditsLabel.innerHTML = "view credits";
+					Credits.appendChild(CreditsLabel);
+				}
+				nonContainer.appendChild(Credits);
 
-                if (Data.Base) {
-                    const Base = document.createElement("span")
-                    Base.style.cursor = "pointer";
-                    Base.addEventListener("click", function () { window.open(Data.Image.slice(0, -5) + "base.webp", "_blank") })
-                    {
-                        const BaseLabel = document.createElement("b");
-                        BaseLabel.innerHTML = "view base";
-                        Base.appendChild(BaseLabel);
-                    }
-                    nonContainer.appendChild(Base);
-                }
-            }
-            Grid.appendChild(nonContainer);
+				if (Data.Base) {
+					const Base = document.createElement("span")
+					Base.style.cursor = "pointer";
+					Base.addEventListener("click", function () { window.open(Data.Image.slice(0, -5) + "base.webp", "_blank") })
+					{
+						const BaseLabel = document.createElement("b");
+						BaseLabel.innerHTML = "view base";
+						Base.appendChild(BaseLabel);
+					}
+					nonContainer.appendChild(Base);
+				}
+			}
+			Grid.appendChild(nonContainer);
         });
     });
 });
 
-function CreditsPopup(Credits) {
+function CreditsPopup(JSON) {
+	let Credits = "";
+	$.each(JSON, function (Author, Creations) {
+		Credits += (Author + "<br>");
+
+		if (Creations.length == 0) return;
+		$.each(Creations, function (_, Creation) {
+			Credits += ("&nbsp;&nbsp;&nbsp;&nbsp;" + Creation + "<br>");
+		});
+	});
+
     const Popup = document.createElement("div");
     Popup.id = "Popup";
     Popup.addEventListener("click", function () { Popup.remove(); });
@@ -79,5 +89,4 @@ function CreditsPopup(Credits) {
         Popup.appendChild(Box);
     }
     Container.appendChild(Popup);
-
 }
